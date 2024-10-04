76320f920cfedd5dc996bc97720d764a74aa1a97
Seshu <scharla@purdue.edu>
2024-10-04 13:17:25 -0400

    chap 1

---
+++ 1_UpAndRunning/abs_sum.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Seshu on 10/4/2024.
+//
+#include <cstdio>
+
+int absolute_value(int x) {
+    if (x < 0) {
+        return -x;
+    }
+    return x;
+}
+
+int sum(int x, int y) {
+    return x + y;
+}
+
+int main() {
+    int x = -5;
+    int y = 10;
+    printf("The absolute value of %d is %d\n", x, absolute_value(x));
+    printf("The sum of %d and %d is %d\n", x, y, sum(x, y));
+    return 0;
+}
